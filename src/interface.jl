8c428c7b6b821b5e6ca5eae8ec32dd98c69f9a53
Mark Chernyshev <mark26111@gmail.com>
2024-02-05 13:11:04 +0100

    add pathevaluation to get_recombination_events

--- src/interface.jl
+++ src/interface.jl
@@ -19,12 +19,12 @@ Get the probability of a sequence being chimeric for each query sequence given a
 get_chimera_probabilities(queries::Vector{String}, references::Vector{String}; fast::Bool = true, mutation_probabilities = [0.02, 0.04, 0.07, 0.11, 0.15], base_mutation_probability = 0.05, prior_probability::Float64 = 1/300) = 
     get_chimera_probabilities(as_ints.(queries), as_ints.(references); fast = fast, mutation_probabilities = mutation_probabilities, base_mutation_probability = base_mutation_probability, prior_probability = prior_probability)
 
-function get_recombination_events(queries::Vector{Vector{Int64}}, references::Vector{Vector{Int64}}; fast::Bool = true, mutation_probabilities = [0.02, 0.04, 0.07, 0.11, 0.15], base_mutation_probability = 0.05, prior_probability::Float64 = 1/300, startingpoint = false)
+function get_recombination_events(queries::Vector{Vector{Int64}}, references::Vector{Vector{Int64}}; fast::Bool = true, mutation_probabilities = [0.02, 0.04, 0.07, 0.11, 0.15], base_mutation_probability = 0.05, prior_probability::Float64 = 1/300, startingpoint = false, pathevaluation = false)
     
     hmm = fast ? ApproximateHMM(vovtomatrix(references), base_mutation_probability, prior_probability) :  FullHMM(vovtomatrix(references), mutation_probabilities, prior_probability)
-    recomb_f(query::Vector{Int64}, hmm::HMM, mutation_probabilities::Vector{Float64}) = startingpoint ? findrecombinations_and_startingpoint(query, hmm, mutation_probabilities) : findrecombinations(query, hmm, mutation_probabilities)
+    recomb_f(query::Vector{Int64}, hmm::HMM, mutation_probabilities::Vector{Float64}) = startingpoint ? pathevaluation ? findrecombinations_and_startingpoint_and_pathevaulation(query, hmm, mutation_probabilities) : findrecombinations_and_startingpoint(query, hmm, mutation_probabilities) : findrecombinations(query, hmm, mutation_probabilities)
 
-    mutation_probabilities = fast ? [base_mutation_probability for i in 1:length(references)] : mutation_probabilities 
+    mutation_probabilities = fast ? [base_mutation_probability for i in eachindex(references)] : mutation_probabilities 
 
     # split query iteration among threads
     recombination_events = Vector(undef, length(queries))
@@ -39,8 +39,8 @@ end
 
 Get the recombination events for a query sequence given a set of reference sequences. The return type is `Vector{NamedTuple{(:position, :at, :to), Int64, Int64, Int64}}`. Each tuple represents a recombination event and is of the form `(position, at, next)`, where `at` and `next` are indices of the references, whilst position is the site of the recombination event. `fast` is a boolean indicating whether to use the approximate HMM or the full HMM. `prior_probability` is the prior probability of a sequence being chimeric.
 """
-get_recombination_events(queries::Vector{String}, references::Vector{String}; fast::Bool = true, mutation_probabilities = [0.02, 0.04, 0.07, 0.11, 0.15], base_mutation_probability = 0.05, prior_probability::Float64 = 1/300, startingpoint = false) = 
-    get_recombination_events(as_ints.(queries), as_ints.(references), fast = fast, mutation_probabilities = mutation_probabilities, base_mutation_probability = base_mutation_probability, prior_probability = prior_probability, startingpoint = startingpoint)
+get_recombination_events(queries::Vector{String}, references::Vector{String}; fast::Bool = true, mutation_probabilities = [0.02, 0.04, 0.07, 0.11, 0.15], base_mutation_probability = 0.05, prior_probability::Float64 = 1/300, startingpoint = false, pathevaluation = false) = 
+    get_recombination_events(as_ints.(queries), as_ints.(references), fast = fast, mutation_probabilities = mutation_probabilities, base_mutation_probability = base_mutation_probability, prior_probability = prior_probability, startingpoint = startingpoint, pathevaluation = pathevaluation)
 
 """
     get_log_site_probabilities(query::String, references::Vector{String}; fast = true, prior_probability = 1/300)
