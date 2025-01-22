6a7d6f75666b1719a987951dd19838f6d40eb78d
aronStalmarck <aron.stalmarck@gmail.com>
2025-01-22 07:55:18 +0100

    (slow) batched BW-chimeraprobability

--- src/batched.jl
+++ src/batched.jl
@@ -29,8 +29,8 @@ function batched_get_bs(hmm::FullHMM, O::AbstractMatrix, mutation_probabilities:
     S = similar(O, UInt32, size(hmm.S)...)
     copy!(S, UInt32.(hmm.S))
     @inbounds for mut in 1:hmm.K # mutation rates
-        same_obs_prob = Float32(1 .- mutation_probabilities[mut])
-        diff_obs_prob = Float32(mutation_probabilities[mut] ./ 3)
+        same_obs_prob = Float32(1 - mutation_probabilities[mut])
+        diff_obs_prob = Float32(mutation_probabilities[mut] / 3)
         for ref in 1:hmm.n # references
             b[ref, mut, :, :] .= get_b.(reshape(S[ref, :], 1, :), O, same_obs_prob, diff_obs_prob)
         end
@@ -38,7 +38,19 @@ function batched_get_bs(hmm::FullHMM, O::AbstractMatrix, mutation_probabilities:
     return b
 end
 
-function get_b(hmm_obs::UInt32, obs::UInt32, same_obs_prob::Float32, diff_obs_prob::Float32)
+function batched_get_bs(hmm::ApproximateHMM, O::AbstractMatrix, mutation_probabilities::AbstractMatrix)
+    b = similar(O, Float32, hmm.N, size(O, 1), hmm.L)
+    S = similar(O, UInt32, size(hmm.S, 1), size(hmm.S, 2))
+    copy!(S, UInt32.(hmm.S))
+    S = reshape(S, :, 1, size(hmm.S, 2))
+    same_obs_prob = reshape(1 .- mutation_probabilities, size(mutation_probabilities)..., 1)
+    diff_obs_prob = reshape(mutation_probabilities ./ 3, size(mutation_probabilities)..., 1)
+
+    b .= get_b.(S, reshape(O, 1, size(O)...), same_obs_prob, diff_obs_prob)
+    return b
+end
+
+function get_b(hmm_obs::UInt32, obs::UInt32, same_obs_prob::Real, diff_obs_prob::Real)
     if (obs == 0x05) | (obs == 0x06)
         return 1.0
     else
@@ -51,15 +63,21 @@ function get_b(hmm_obs::UInt32, obs::UInt32, same_obs_prob::Float32, diff_obs_pr
 end
 
 # ==== Batched algorithms.jl ==== #
-function chimeraprobability(device::Function, O::Vector{Vector{UInt8}}, hmm::T, mutation_probabilities::Vector{Float64}) where T <: FullHMM
+function chimeraprobability(device::Function, O::Vector{Vector{UInt8}}, hmm::T, mutation_probabilities::Vector{Float64}) where T <: HMM
     O = device(UInt32.(vovtomatrix(O)))
-    b = batched_get_bs(hmm, O, mutation_probabilities)
-    #T == ApproximateHMM && parameterestimation!(hmm, O, mutation_probabilities, b)
-    #b = T == ApproximateHMM ? get_bs(hmm, O, mutation_probabilities) : b
+    if hmm isa ApproximateHMM
+        mutation_probabilities_matrix = device(repeat(mutation_probabilities, 1, size(O, 1)))
+        b = batched_get_bs(hmm, O, mutation_probabilities_matrix)
+        parameterestimation!(hmm, O, mutation_probabilities_matrix, b)
+        b = hmm isa ApproximateHMM ? batched_get_bs(hmm, O, mutation_probabilities_matrix) : b
+    else
+        b = batched_get_bs(hmm, O, mutation_probabilities)
+    end
     x = forward(hmm, b) |> Vector
     return x
 end
 
+# == FullHMM == #
 function forward(hmm::FullHMM, b::AbstractArray{T}) where T <: Real
     a_self = T(1 - hmm.switch_probability - hmm.μ)
     a_diffref = T(hmm.switch_probability / ((hmm.n - 1) * hmm.K))
@@ -88,8 +106,97 @@ function forward(hmm::FullHMM, b::AbstractArray{T}) where T <: Real
     end
 
     detec = sum(alpha[2, :, :, :], dims = (1, 2))
-    return reshape(detec ./ (detec .+ sum(alpha[1, :, :, :], dims = (1, 2))), :)
+    return vec(detec ./ (detec .+ sum(alpha[1, :, :, :], dims = (1, 2))))
 end 
 
-#stateindicesofref_bounds(ref_idx::Integer, hmm::FullHMM) = (1 + (ref_idx-1) * hmm.K, ref_idx * hmm.K)
-#stateindicesofref_bounds(ref_idx::Integer, hmm::ApproximateHMM) = (ref_idx, ref_idx)
+
+# == ApproximateHMM == #
+
+function forward(hmm::ApproximateHMM, b::AbstractArray{T}) where T <: Real
+    a_false = T(a(false, hmm))
+    a_true = T(a(true, hmm))
+
+    batchdim = size(b, 2)
+
+    alpha = similar(b, 2, hmm.N, batchdim)
+
+    alpha[1, :, :] .= T(initialstate(hmm)) .* b[:, :, 1]
+    alpha[2, :, :] .= zero(T)
+
+    postchimera_mask = similar(alpha, 2, 1)
+    postchimera_mask[1, :, :] .= zero(T)
+    postchimera_mask[2, :, :] .= a_false
+
+    for t in 1:hmm.L-1
+        sumalpha = sum(alpha, dims = (1, 2))
+        sumref = sum(alpha, dims=1)
+
+        alpha .= (alpha .* a_true .+ (sumalpha .- sumref) .* postchimera_mask) .* reshape(b[:, :, t+1], 1, hmm.N, :)
+        
+        alpha ./= sum(alpha, dims=(1, 2, 3))
+    end
+
+    detec = sum(alpha[2, :, :], dims = 1)
+    return vec(detec ./ (detec .+ sum(alpha[1, :, :], dims = 1)))
+end
+
+function forward!(alpha::AbstractArray, c::AbstractMatrix, hmm::ApproximateHMM, b::AbstractArray{T}) where T <: Real
+    a_false = T(a(false, hmm))
+    a_true = T(a(true, hmm))
+
+    c[:, 1] .= one(T)
+
+    alpha[:, :, 1] .= T(initialstate(hmm)) .* b[:, :, 1]
+
+    for t in 1:hmm.L-1
+        sumalpha = sum(alpha[:, :, t], dims = 1)
+
+        alpha[:, :, t+1] .= (alpha[:, :, t] .* a_true .+ (sumalpha .- alpha[:, :, t]) .* a_false) .* b[:, :, t+1]
+        
+        c[:, t+1] .= one(T) ./ vec(sum(alpha[:, :, t+1], dims=1))
+        alpha[:, :, t+1] .*= reshape(c[:, t + 1], 1, :)
+    end
+end
+
+function backward!(beta::AbstractArray, c::AbstractMatrix, hmm::ApproximateHMM, b::AbstractArray{T}) where T <: Real
+    a_false = a(false, hmm)
+    a_true = a(true, hmm)
+
+    beta[:, :, hmm.L] .= reshape(c[:, hmm.L], 1, :)
+    for t in hmm.L-1:-1:1
+        sumbeta = sum(beta[:, :, t+1] .* b[:, :, t+1], dims=1)   #, dims = 1)
+        beta[:, :, t] .= a_false .* (sumbeta .- beta[:, :, t+1] .* b[:, :, t+1]) .+ a_true .* beta[:, :, t+1] .* b[:, :, t+1]
+        beta[:, :, t] .*= reshape(c[:, t], 1, :)
+    end
+end
+
+function parameterestimation!(hmm::ApproximateHMM, O::AbstractMatrix, mutation_probabilities::AbstractMatrix, b::AbstractArray{T}) where T <: Real
+    batchsize = size(O, 1)
+    alpha = similar(b, hmm.N, batchsize, hmm.L)
+    beta = similar(b, hmm.N, batchsize, hmm.L)
+    c = similar(b, batchsize, hmm.L)
+    forward!(alpha, c, hmm, b)
+    backward!(beta, c, hmm, b)
+	Nmut = similar(b, hmm.N, batchsize)
+    Nsame = similar(b, hmm.N, batchsize)
+
+    # pseudocount prior
+    Nmut .= T(2)
+    Nsame .= T(10)
+
+    C = alpha .* beta
+    sumC = sum(C, dims = 1)
+
+    S = similar(O, UInt32, size(hmm.S)...)
+    copy!(S, UInt32.(hmm.S))
+
+    for t in 1:hmm.L
+        Ot = reshape(O[:, t], 1, :)
+        St = reshape(S[:, t], :, 1)
+
+        # Ot.!= 6 to handle non-informative obs
+        Nmut .+= (Ot.!= T(6)) .* (Ot .!= St) .* alpha[:, :, t] .* beta[:, :, t] ./ sumC[:, :, t]
+        Nsame .+= (Ot .!= T(6)) .* (Ot .== St) .* alpha[:, :, t] .* beta[:, :, t] ./ sumC[:, :, t]
+    end
+    mutation_probabilities .= Nmut ./ (Nmut .+ Nsame)
+end
